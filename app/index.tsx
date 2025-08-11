eb0ef928a7e026044880aeea44e4e7d773361fab
AvG-Aayush <avg.gaming.np@gmail.com>
2025-08-11 12:33:55 +0545

    start

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+const Home = () => {
+return (
+    <View className="flex-1 items-center justify-center bg-zinc-600">
+        <Text className="text-white">Home</Text>
+    </View>
+)
+}
+export default Home;
